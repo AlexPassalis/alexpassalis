54f851530d4b80284158a2e544bef4cbe02f480d
AlexPassalis <alexpassalis9@gmail.com>
2025-01-19 11:32:54 +0200

    deploy.sh changes

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,22 @@ docker rm container-nginx container-next-js-1 container-next-js-2 container-next
 docker rmi image-nginx image-next-js
 docker network rm network-alexpassalis_com
 
-# Remove old directory
-sudo rm -r alexpassalis_com
-
 # Setup SSH agent
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519_alexpassalis.com
+
+# Save script to temp location
+cp deploy.sh /tmp/deploy.sh
+
+# Go to parent directory and remove old directory
+cd ..
+sudo rm -r alexpassalis_com
 
 # Clone and build
 git clone git@github.com:AlexPassalis/alexpassalis_com.git
 cd alexpassalis_com
 docker build --target runner -t image-next-js ./front-end/next-js
-docker compose up --build -d 
+docker compose up --build -d
+
+# Clean up
+rm /tmp/deploy.sh 
