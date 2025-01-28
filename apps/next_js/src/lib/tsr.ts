f7d9f08b4ea13fc198e146eb29c89b4e6ff0824c
AlexPassalis <alexpassalis9@gmail.com>
2025-01-28 11:15:32 +0200

    + console.log

--- apps/next_js/src/lib/tsr.ts
+++ apps/next_js/src/lib/tsr.ts
@@ -1,6 +1,8 @@
 import { initTsrReactQuery } from '@ts-rest/react-query/v5'
 import contract from './../../../contract/src/main'
 
+console.log(process.env.NEXT_PUBLIC_FASTIFY_BASE_URL)
+
 export const tsr = initTsrReactQuery(contract, {
   baseUrl: process.env.NEXT_PUBLIC_FASTIFY_BASE_URL as string,
   baseHeaders: {
