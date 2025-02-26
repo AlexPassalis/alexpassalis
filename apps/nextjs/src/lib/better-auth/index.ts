fcbc18bab30a44c70de572d16eeac5c73910d01d
AlexPassalis <alexpassalis9@gmail.com>
2025-02-26 23:15:37 +0200

    removed .env from nextjs

--- apps/nextjs/src/lib/better-auth/index.ts
+++ apps/nextjs/src/lib/better-auth/index.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
-import env from '@/env'
 import { emailOTPClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [emailOTPClient()],
 })
