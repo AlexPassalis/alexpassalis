858e92591413e7d092da20e0edd464c0bafae3be
AlexPassalis <alexpassalis9@gmail.com>
2025-02-26 20:08:24 +0200

    pkg for Pool

--- apps/hono/src/lib/postgres/index.ts
+++ apps/hono/src/lib/postgres/index.ts
@@ -1,4 +1,5 @@
-import { Pool } from 'pg'
+import pkg from 'pg'
+const { Pool } = pkg
 import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from '@/lib/postgres/schema'
 import { logger } from '@/config/logger'
