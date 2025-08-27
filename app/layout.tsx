baf8fb7ae330c929875b1bba976e6d86c25d750b
AlexPassalis <alexpassalis9@gmail.com>
2025-08-27 14:04:17 +0200

    Fix favicon not showing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css'
 
+import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [{ url: '/icon.svg', type: 'image/svg+xml' }, { url: '/icon.svg' }],
+  },
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
